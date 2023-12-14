56805f7b3b9aad29974936504a4e405c21ebcf11
Qayyum Siddiqui <54232882+qayyum22@users.noreply.github.com>
2023-12-14 10:05:13 +0530

    Update Contact.js

--- routes/Contact.js
+++ routes/Contact.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 // import handlers
 const {contact} = require("../controllers/ContactUs");
+const {testing} = require("../controllers/ContactUs");
 
 //router
 router.post("/contact", contact);
+router.get("/testing", testing);
 
 module.exports = router;
