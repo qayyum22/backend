7668d088ffe888eb27b1b30a09e79664a824ed62
Qayyum Siddiqui <54232882+qayyum22@users.noreply.github.com>
2023-12-14 10:27:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000"],
+		origin: ["http://localhost:3000","https://frontend-qayyum22.vercel.app"],
 		credentials: true,
 		allowedHeaders: ["Content-Type", "Authorization"]
 	})
