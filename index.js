0fb56191565c764d8b0ce9cb6feaa22e5476c1af
Qayyum Siddiqui <54232882+qayyum22@users.noreply.github.com>
2023-12-14 08:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "https://sblab-frontend-qayyum22.vercel.app",
+		origin: ["https://frontend-qayyum22.vercel.app", "http://localhost:3000", "https://sblab-frontend-qayyum22.vercel.app",
 			"https://sblab-frontend.vercel.app",
 			 "https://sblab-frontend-git-main-qayyum22.vercel.app"],
 		credentials: true,
